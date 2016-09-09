5eadad4218c5a16557a54d2d6729ac241f17d1fc
LeandroGuilherme <leandro-gf@uol.com.br>
2016-09-08 21:59:24 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -20,6 +20,16 @@ FILA novaFila() {
   return f;
 }
 
+void inseriresq(FILA f,int e) {
+  if(f->maisNovo == NULL) {
+    f->maisAntigo = f->maisNovo = novoNo(e, NULL);
+  } else {
+    f->maisAntigo->prev = novoNo(e, NULL);
+    f->maisAntigo = f->maisAntigo->prev;
+  }
+}
+
+
 void inserir(FILA f, int e) {
   if(f->maisAntigo == NULL) {
     f->maisAntigo = f->maisNovo = novoNo(e, NULL);
@@ -28,6 +38,26 @@ void inserir(FILA f, int e) {
     f->maisNovo = f->maisNovo->next;
   }
 }
+
+int removerdir(FILA f){
+  int x;
+  link t;
+  if(filaVazia(f)){
+    printf ("Erro, a fila esta vazia\n");
+    return 0;
+  }
+  
+  x = f->maisNovo->item;
+  t = f->maisNovo;
+  f->maisNovo = f->maisNovo->prev;
+ 
+  if(f->maisNovo == NULL)
+    f->maisAntigo = NULL;
+
+  free(t);
+  return x;
+}
+
 
 int remover(FILA f){
   int x;
